ab6437b39d70b316b7ae7e7d5710a78eb8486ee7
cis <cis@CISM532>
2019-06-28 14:51:45 +0530

    ok

---
+++ src/test/java/MDMethods/NavigationMethods.java
@@ -0,0 +1,197 @@
+package MDMethods;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import MDElements.Md_Element;
+
+public class NavigationMethods {
+
+public static WebDriver driver;
+public static Properties prop;
+public static FileInputStream inputFile;
+	
+
+public static WebDriver OpenBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		return driver;
+	}
+	
+	public static void Openurlandlogin ()
+	{
+		driver.get("https://next.testmd.co.uk/login");
+		driver.findElement(By.id("login-username")).sendKeys("ms1@reconmail.com");
+		driver.findElement(By.id("login-password")).sendKeys("123456789");
+		driver.findElement(By.name("commit")).click();
+		
+	}
+	
+	public static WebDriver Signup ()
+	{
+		
+		driver.get("https://next.testmd.co.uk/signup");
+		if(driver.getPageSource().contains("CHOOSE YOUR ACCOUNT TYPE"))
+		{
+		System.out.println("Text is Present");
+		}
+		else
+		{
+		System.out.println("Text is not Present");
+		}
+		driver.findElement(By.id("role-id-2")).click();
+		driver.findElement(By.name("commit")).click();
+		return driver;
+		
+	}
+
+	public static void getData()
+	{
+		
+		File file= new File("D:\\Selinium Ecllipse\\MD\\src\\test\\java\\TestNgScripts\\marketdojo.properties");
+		FileInputStream fileIn= null;
+		try {
+			fileIn = new FileInputStream(file);
+		    //Create properties Class object 
+		     prop = new Properties();
+		    prop.load(fileIn);
+		       }
+		   
+		   catch (Exception e)
+		   {
+		    e.printStackTrace();
+		   }
+	}
+			                                                           // Quick Quote Methods 
+
+		public static void QQlogin(){
+	        driver.get("https://next.testmd.co.uk/login");
+	        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.findElement(By.id("login-username")).sendKeys("sawa@99price.co");
+			driver.findElement(By.id("login-password")).sendKeys("123456789");
+			driver.findElement(By.name("commit")).click();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.findElement(By.xpath( "//*[@id='app']/div/div/div/div/div/div/div/div[2]/div[2]/a")).click();
+	       }
+		
+		
+		public static void QQlogout() throws InterruptedException{
+			WebDriverWait wait = new WebDriverWait(NavigationMethods.driver, 3000);
+		    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Md_Element.headerProfile)));
+			NavigationMethods.driver.findElement(By.xpath(Md_Element.headerProfile)).click();
+			WebDriverWait wait1 = new WebDriverWait(NavigationMethods.driver, 3000);
+		    wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Md_Element.headerdropLogout)));
+			NavigationMethods.driver.findElement(By.xpath(Md_Element.headerdropLogout)).click();
+		}
+		
+		public static void DirectGetQuote () throws InterruptedException {
+			Thread.sleep(8000);
+			JavascriptExecutor jse = (JavascriptExecutor)NavigationMethods.driver;
+			jse.executeScript("window.scrollBy(0,1000)", "400"); 
+			NavigationMethods.driver.findElement(By.xpath(Md_Element.qqGetquote)).click();
+			WebDriverWait wait = new WebDriverWait(NavigationMethods.driver, 20);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Md_Element.qqdisGetquote)));
+			Assert.assertFalse(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqdisGetquote)).isEnabled());
+		}
+		
+		public static void QQName (){
+		   WebDriverWait wait = new WebDriverWait(NavigationMethods.driver, 3000);
+		   wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(Md_Element.qqName)));
+		   NavigationMethods.driver.findElement(By.id(Md_Element.qqName)).sendKeys("Automate");
+		}
+		
+		public static void QQchecklot () throws InterruptedException{
+			NavigationMethods.QQName();
+			Thread.sleep(3000);
+			JavascriptExecutor jse = (JavascriptExecutor)NavigationMethods.driver;
+			jse.executeScript("window.scrollBy(0,250)", "400");
+			NavigationMethods.driver.findElement(By.xpath(Md_Element.qqPricechekbox)).click();
+		}
+		
+		
+		public static void Scroll(){
+			JavascriptExecutor jse = (JavascriptExecutor)NavigationMethods.driver;
+			jse.executeScript("window.scrollBy(0,250)", "400"); 			
+		}
+		
+	    public static void QQCreate1lot () throws InterruptedException{
+	    	NavigationMethods.QQchecklot();
+			NavigationMethods.driver.findElement(By.name(Md_Element.qqLotName0)).sendKeys("0");
+			WebDriverWait wait = new WebDriverWait(NavigationMethods.driver, 3000);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.name(Md_Element.qqUom0)));
+			Thread.sleep(3000);
+			NavigationMethods.driver.findElement(By.name(Md_Element.qqUom0)).sendKeys("mm");
+			NavigationMethods.driver.findElement(By.name(Md_Element.qqQuantity0)).sendKeys("10");
+			NavigationMethods.driver.findElement(By.xpath(Md_Element.qqAddlot0)).click();
+		}
+		
+		
+		
+	    public static void QQCreate5lot () throws InterruptedException{                   
+		NavigationMethods.QQCreate1lot ();
+		Thread.sleep(3000);
+		WebDriverWait wait1 = new WebDriverWait(NavigationMethods.driver, 3000);
+		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.name(Md_Element.qqLotName1)));
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqLotName1)).sendKeys("1");
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqUom1)).sendKeys("mm");
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQuantity1)).sendKeys("10");
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqAddlot1)).click();
+		Thread.sleep(3000);
+//		JavascriptExecutor jse1 = (JavascriptExecutor)NavigationMethods.driver;
+//		jse1.executeScript("window.scrollBy(0,350)", "400");
+	      ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+		WebDriverWait wait2 = new WebDriverWait(NavigationMethods.driver, 10);
+		wait2.until(ExpectedConditions.visibilityOfElementLocated(By.name(Md_Element.qqLotName2)));
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqLotName2)).sendKeys("2");
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqUom2)).sendKeys("mm");
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQuantity2)).sendKeys("10");
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqAddlot2)).click();
+		WebDriverWait wait3 = new WebDriverWait(NavigationMethods.driver, 10);
+		wait3.until(ExpectedConditions.visibilityOfElementLocated(By.name(Md_Element.qqLotName3)));
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqLotName3)).sendKeys("3");
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqUom3)).sendKeys("mm");
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQuantity3)).sendKeys("10");
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqAddlot3)).click();
+		WebDriverWait wait4 = new WebDriverWait(NavigationMethods.driver, 10);
+		wait4.until(ExpectedConditions.visibilityOfElementLocated(By.name(Md_Element.qqLotName4)));
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqLotName4)).sendKeys("4");
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqUom4)).sendKeys("mm");
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQuantity4)).sendKeys("10");
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqAddlot4)).click();
+		
+		WebDriverWait wait5 = new WebDriverWait(NavigationMethods.driver, 10);
+	    wait5.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Md_Element.qqvalLotMax)));
+		Assert.assertEquals(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqvalLotMax)).getText(), "only 5 lots are allowed");
+	    }
+	    
+	    
+	    public static void QQGetquote(){
+	    	 NavigationMethods.driver.findElement(By.xpath(Md_Element.qqGetquote)).click();			
+		}
+	    
+	    
+	    
+}
+		
+	    
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/TestNgScripts/QuickQuotes.java
@@ -0,0 +1,258 @@
+package TestNgScripts;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;import org.apache.poi.ss.formula.functions.Na;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.Select;
+
+import MDElements.Md_Element;
+import MDMethods.NavigationMethods;
+
+public class QuickQuotes extends ExtentReportGenerate {
+	
+	
+	
+	@BeforeClass
+	public void openBrowser()
+	{
+		NavigationMethods.OpenBrowser();
+	}
+	
+	@BeforeMethod
+	public void openApplication()
+	{
+		 
+		NavigationMethods.QQlogin();
+	}
+	
+	
+	@Test
+	public void QQByName()                                                                                                                           // VERIFY CREATE QUICK QUOTE BY EVENT NAME 
+	{
+		test=extent.createTest("QQByName");
+		  WebDriverWait wait = new WebDriverWait(NavigationMethods.driver, 20);
+		  wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(Md_Element.qqName)));
+		  NavigationMethods.driver.findElement(By.id(Md_Element.qqName)).sendKeys("Automate");
+		  WebDriverWait wait2 = new WebDriverWait(NavigationMethods.driver, 20);
+		  wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Md_Element.qqGetquote)));
+		  NavigationMethods.driver.findElement(By.xpath(Md_Element.qqGetquote)).click();
+	}
+	
+	@Test                                                                                                                                          // VERIFY Validation Message on EVENT NAME 
+	public void VerifymsgonEventname() throws InterruptedException
+	{
+	    test=extent.createTest("VerifymsgonEventname");
+	     WebDriverWait wait = new WebDriverWait(NavigationMethods.driver, 5000);
+	     wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(Md_Element.qqName)));
+         NavigationMethods.driver.findElement(By.id(Md_Element.qqName)).click();
+         NavigationMethods.driver.findElement(By.id(Md_Element.qqDeadline)).click();
+         WebDriverWait wait2 = new WebDriverWait(NavigationMethods.driver, 5000);
+	     wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Md_Element.qqvalName1)));
+	     Thread.sleep(5000);
+         NavigationMethods.driver.findElement(By.id(Md_Element.qqName)).sendKeys("Using the system will allow you to benefit from tes");
+         NavigationMethods.driver.findElement(By.id(Md_Element.qqDeadline)).click();
+         WebDriverWait wait3 = new WebDriverWait(NavigationMethods.driver, 5000);
+	     wait3.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Md_Element.qqvalName)));
+         Assert.assertEquals(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqvalName)).getText(), "Name too long (can not be greater than 50 characters)");
+	}
+	
+	@Test
+	public void VerifyGetquotedisLotname() throws InterruptedException{                                                              //VERIFY GET QUOTE BUTTON IS DISABLE  WITHOUT ENETR LOT NAME 
+		test=extent.createTest("VerifyGetquotedis");
+		NavigationMethods.DirectGetQuote();	
+		Assert.assertFalse(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqdisGetquote)).isEnabled());
+	}
+	
+	@Test                                                                                                                                               // VERIFY DATE FIELD 
+	public void VerifyDate() throws InterruptedException{
+		test=extent.createTest("VerifyDate");
+		NavigationMethods.driver.findElement(By.id(Md_Element.qqName)).sendKeys("Test");
+		NavigationMethods.driver.findElement(By.id(Md_Element.qqDeadline)).sendKeys("2019-05-27 12:55");
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqGetquote)).click();
+		Assert.assertEquals(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqToasterr)).getText(), "THE DEADLINE YOU HAVE SELECTED IN THE PAST. PLEASE SELECT A DEADLINE DATE IN THE FUTURE.");
+		NavigationMethods.driver.findElement(By.id(Md_Element.qqDeadline)).clear();
+		NavigationMethods.driver.findElement(By.id(Md_Element.qqDeadline)).sendKeys("2020-05-27 12:55");
+		NavigationMethods.driver.findElement(By.id(Md_Element.qqDeadline)).click();
+	}
+	
+	  @Test                                                                                                                                     //VERIFY DESCRIPTION
+	  public void VerifyDes() throws InterruptedException{
+		  test=extent.createTest("VerifyDate");
+		  NavigationMethods.QQName();
+		  Thread.sleep(5000);
+		  NavigationMethods.driver.switchTo().frame(0);
+		  NavigationMethods.driver.findElement(By.xpath(Md_Element.qqDes)).sendKeys("Lorem Ipsum is simply dummy text of the printing");
+		  Thread.sleep(5000);
+		  NavigationMethods.driver.switchTo().defaultContent();
+		  NavigationMethods.QQGetquote();
+	  }
+	
+	  @Test                                                                                                                                     //VERIFY DOC UPLOAD 
+	  public void VerifyDocupload() throws IOException, InterruptedException{
+		  test=extent.createTest("VerifyDocupload");
+		  NavigationMethods.QQName();
+		  ((JavascriptExecutor) NavigationMethods.driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+		  Thread.sleep(5000);
+		  NavigationMethods.driver.findElement(By.xpath(Md_Element.qqDrop)).click();
+		  Thread.sleep(5000);
+		  Runtime.getRuntime().exec("C:\\Users\\cis1\\Desktop\\AutoIT\\Gaurav_QQ1.exe");
+	  }
+	  
+
+	@Test                                                                                                                                          // VERIFY ADDING 5 LOTS 
+	public void Verfiyadding5lots() throws InterruptedException{
+		test=extent.createTest("Verfiyadding5lots");
+	NavigationMethods.QQCreate5lot();
+	}
+	
+	
+	@Test
+	public void VerifymsgonLotname() throws InterruptedException{                                                                        // VERIFY Validation MESSAGE on LOT NAME (MIN & MAX)
+		test=extent.createTest("VerifymsgonLotname");
+		NavigationMethods.QQchecklot();
+		Assert.assertEquals(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqvalLotnamereq)).getText(), "Required");
+		Thread.sleep(3000);
+		NavigationMethods.Scroll();
+		Thread.sleep(3000);
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqLotName0)).sendKeys("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.");
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqUom0)).click();
+		Thread.sleep(3000);
+		WebDriverWait wait = new WebDriverWait(NavigationMethods.driver, 2000);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Md_Element.qqvalLotnamemax)));
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqUom0)).click();
+		Assert.assertEquals(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqvalLotnamemax)).getText(), "Lot Name too long (can not be greater than 150 characters)");
+	}
+	
+	
+	@Test
+	    public void VerifymsgonQuantity() throws InterruptedException{                                                                                       //VERIFY VALIDATION MESSAGE ON QUANTITY 
+		test=extent.createTest("VerifymsgonQuantity");
+		NavigationMethods.QQchecklot();
+		NavigationMethods.Scroll();
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQuantity0)).sendKeys("Test");
+		Thread.sleep(3000);
+		Assert.assertEquals(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqvalLotquanum)).getText(), "Should be a valid number");
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQuantity0)).clear();
+		WebDriverWait wait= new WebDriverWait(NavigationMethods.driver, 3000);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name(Md_Element.qqQuantity0)));
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQuantity0)).sendKeys("9999999999999");
+		Assert.assertEquals(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqvalLotquamax)).getText(), "Quantity must be less than 999999999999.99");
+		
+	}
+	
+	@Test
+	public void VerifyonDeltelot() throws InterruptedException{                                                                                           //VERIFY DELETE LOT 
+		test=extent.createTest("VerifyonDeltelot");
+		NavigationMethods.QQCreate1lot();
+		WebDriverWait wait = new WebDriverWait(NavigationMethods.driver, 4000);
+     	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Md_Element.qqDelete0)));
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqDelete0)).click();
+	}
+
+		
+	@Test
+	public void VerifyGetquotedisQuesLot(){                                                                                             //VERIFY GET QUOTE BUTTON IS DISABLE WHEN QUESTIONNAIRE & LOT IS CHECKED  
+		test=extent.createTest("VerifyGetquotedisQuesLot");
+		NavigationMethods.driver.findElement(By.id(Md_Element.qqName)).sendKeys("Test");                                            
+		NavigationMethods.Scroll(); 
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqPricechekbox)).click();
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqQuecheckbox)).click();
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqGetquote)).click();
+		WebDriverWait wait = new WebDriverWait(NavigationMethods.driver, 2000);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Md_Element.qqdisGetquote)));
+		Assert.assertFalse(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqdisGetquote)).isEnabled());
+	}
+	
+	
+	
+	@Test 
+	public void AddingQuestion() throws InterruptedException{                                                                                           //Verify Adding QUestion 
+		test=extent.createTest("AddingQuestion");
+		NavigationMethods.QQName();
+		((JavascriptExecutor) NavigationMethods. driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+		Thread.sleep(3000);
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqQuecheckbox)).click();
+		WebDriverWait wait = new WebDriverWait(NavigationMethods.driver, 4000);
+     	wait.until(ExpectedConditions.visibilityOfElementLocated(By.name(Md_Element.qqQueName0)));
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQueName0)).sendKeys("hi");
+		WebDriverWait wait1 = new WebDriverWait(NavigationMethods.driver,4000);
+		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.name(Md_Element.qqQuesDrop)));
+		Select dropdown = new Select(NavigationMethods.driver.findElement(By.name(Md_Element.qqQuesDrop)));
+		dropdown.selectByValue("one_line_text");
+		Thread.sleep(3000);
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqAddBtn0)).click();
+		Thread.sleep(3000);
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQueName1)).sendKeys("hi");
+		WebDriverWait wait2 = new WebDriverWait(NavigationMethods.driver, 4000);
+		wait2.until(ExpectedConditions.visibilityOfElementLocated(By.name(Md_Element.qqQuesDrop1)));
+		Select dropdown1 = new Select(NavigationMethods.driver.findElement(By.name(Md_Element.qqQuesDrop1)));
+		dropdown1.selectByValue("document_upload");
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqAddBtn1)).click();
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQueName2)).sendKeys("hi");
+		WebDriverWait wait5=new WebDriverWait(NavigationMethods.driver,4000);
+		wait5.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.name(Md_Element.qqQuesDrop2)));
+		Select dropdown2 = new Select(NavigationMethods.driver.findElement(By.name(Md_Element.qqQuesDrop2)));
+		dropdown2.selectByValue("yes_no");
+	}
+	
+	@Test
+	public void VerifymsgonQues() throws InterruptedException{                                                                           //Verify Validation Messages on Questions
+		test=extent.createTest("VerifymsgonQues");
+		NavigationMethods.QQName();
+		Thread.sleep(5000);
+		((JavascriptExecutor) NavigationMethods. driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+		Thread.sleep(5000);
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqQuecheckbox)).click();
+		Assert.assertEquals(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqValQuenamereq)).getText(), "Required");
+		Assert.assertEquals(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqValQuetype)).getText(), "Required");
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQueName0)).sendKeys("Any detailed information about the product which will help you to assess the quoteAny detailed information about the product which will help you to assess the quoteAny detailed information about the product which will help you to assess the quoteAsad werer");
+        Assert.assertEquals(NavigationMethods.driver.findElement(By.xpath(Md_Element.qqValQuenamemax)).getText(), "Title too long (can not be greater than 255 characters)");
+	}
+	
+	
+	@Test                                                                                                                         //Verify Delete Questions
+	public void VerifyDelQues() throws InterruptedException{
+		test=extent.createTest("VerifyDelQues");
+		NavigationMethods.QQName();
+		((JavascriptExecutor) NavigationMethods. driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
+		Thread.sleep(3000);
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqQuecheckbox)).click();
+		WebDriverWait wait = new WebDriverWait(NavigationMethods.driver, 4000);
+     	wait.until(ExpectedConditions.visibilityOfElementLocated(By.name(Md_Element.qqQueName0)));
+		NavigationMethods.driver.findElement(By.name(Md_Element.qqQueName0)).sendKeys("hi");
+		WebDriverWait wait1 = new WebDriverWait(NavigationMethods.driver,4000);
+		wait1.until(ExpectedConditions.visibilityOfElementLocated(By.name(Md_Element.qqQuesDrop)));
+		Select dropdown = new Select(NavigationMethods.driver.findElement(By.name(Md_Element.qqQuesDrop)));
+		dropdown.selectByValue("one_line_text");
+		NavigationMethods.driver.findElement(By.xpath(Md_Element.qqQuedelete1)).click();
+	}
+
+	
+	
+	  @AfterMethod
+	  public void ReturnToHome() throws InterruptedException{
+		
+			NavigationMethods.QQlogout();
+		  
+	  }
+
+	  @AfterClass
+	  public void CloseBrowser(){
+		  NavigationMethods.driver.close();  
+	  }
+	  
+	}
+
